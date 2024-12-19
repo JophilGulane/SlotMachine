4ced017a925fc67d45504942650709c6a6cdbc34
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-12-20 02:09:25 +0800

    asd

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,11 +52,14 @@
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(92, 79);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
+
+
             pictureBox2.BackgroundImageLayout = ImageLayout.None;
             pictureBox2.Location = new Point(323, 237);
             pictureBox2.Name = "pictureBox2";
